4455fe36b8bcf404897435e16d5ebfb1ef153284
isaacngabwali <isaacngabwali@gmail.com>
2023-07-09 19:01:25 +0300

    hive

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports={
-  PORT: process.env.PORT || 4000,
+  PORT: process.env.PORT || 4001,
   MONGODB: process.env.MONGODB || 'mongodb+srv://fanitebase:fanitebase@cluster0.f2ptyfi.mongodb.net/?retryWrites=true&w=majority',
   SECRET_KEY: process.env.SECRET_KEY || 'GEW.w3VFb5sH$9OTIDfSwFqIJmOujgQOJMofpjJqCWc9JVrzZiHEy1z8pGEu84d'
 }
